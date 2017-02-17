3e448e8f99b795b2380890f5ef0d05c2783f802f
Simon Cropp <simon.cropp@gmail.com>
2017-02-17 13:35:06 +1100

    add assembly copywrite

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,3 +2,5 @@
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
+[assembly: AssemblyCompany("NServiceBus Ltd.")]
