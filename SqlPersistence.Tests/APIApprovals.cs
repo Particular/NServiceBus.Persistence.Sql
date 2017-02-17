0ec649cf2be9ccadeb15e45580c4476589f3cb5b
Simon Cropp <simon.cropp@gmail.com>
2017-02-17 14:01:23 +1100

    add api approvals

---
+++ SqlPersistence.Tests/APIApprovals.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using ApprovalTests;
+using NUnit.Framework;
+using PublicApiGenerator;
+
+[TestFixture]
+public class APIApprovals
+{
+    [Test]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void Approve()
+    {
+        var combine = Path.Combine(TestContext.CurrentContext.TestDirectory, "NServiceBus.Persistence.Sql.dll");
+        var assembly = Assembly.LoadFile(combine);
+        var publicApi = Filter(ApiGenerator.GeneratePublicApi(assembly));
+        Approvals.Verify(publicApi);
+    }
+
+    string Filter(string text)
+    {
+        return string.Join(Environment.NewLine, text.Split(new[]
+        {
+            Environment.NewLine
+        }, StringSplitOptions.RemoveEmptyEntries)
+            .Where(l => !l.StartsWith("[assembly: ReleaseDateAttribute("))
+            .Where(l => !string.IsNullOrWhiteSpace(l))
+            );
+    }
+
+}
