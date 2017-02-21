13f79cda93fc2f56f8a1b88b14791de979480aa0
Simon Cropp <simon.cropp@gmail.com>
2017-02-21 14:24:01 +1100

    add missing file

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
+[assembly: AssemblyCompany("NServiceBus Ltd.")]
