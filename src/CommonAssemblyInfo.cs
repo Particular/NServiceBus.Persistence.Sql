89b1ee0f117c4426ca0ea78f61a5683e6f8b876a
Simon Cropp <simon.cropp@gmail.com>
2017-02-22 15:51:29 +1100

    remove Assembly attributes

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
 [assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
 [assembly: AssemblyCompany("NServiceBus Ltd.")]
