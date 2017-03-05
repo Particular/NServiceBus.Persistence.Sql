fa43c787a5bbe12ff01f6b9f62b50946ac710ad6
Simon Cropp <simon.cropp@gmail.com>
2017-03-06 10:11:54 +1100

    SqlSagaAttribute prop should be read only

--- src/SqlPersistence/Saga/SqlSagaAttribute.cs
+++ src/SqlPersistence/Saga/SqlSagaAttribute.cs
@@ -5,9 +5,9 @@ namespace NServiceBus.Persistence.Sql
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
     public class SqlSagaAttribute : Attribute
     {
-        public string CorrelationProperty;
-        public string TransitionalCorrelationProperty;
-        public string TableSuffix;
+        public string CorrelationProperty { get; }
+        public string TransitionalCorrelationProperty { get; }
+        public string TableSuffix { get; }
 
         public SqlSagaAttribute(string correlationProperty = null, string transitionalCorrelationProperty = null, string tableSuffix = null)
         {
