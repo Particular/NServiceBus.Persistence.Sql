6a7a20c5bb18c23115d70c9960133de454e0cbc7
Simon Cropp <simon.cropp@gmail.com>
2017-05-09 12:47:49 +1000

    remove Resourcer

--- src/ScriptBuilder/ResourceReader.cs
+++ src/ScriptBuilder/ResourceReader.cs
@@ -1,10 +1,16 @@
-﻿using NServiceBus.Persistence.Sql.ScriptBuilder;
-using Resourcer;
+﻿using System.IO;
+using NServiceBus.Persistence.Sql.ScriptBuilder;
 
 static class ResourceReader
 {
     public static string ReadResource(BuildSqlVariant sqlVariant, string prefix)
     {
-        return Resource.AsStringUnChecked($"NServiceBus.Persistence.Sql.{prefix}_{sqlVariant}.sql");
+        var text = $"NServiceBus.Persistence.Sql.{prefix}_{sqlVariant}.sql";
+        using (var stream = typeof(ResourceReader).Assembly.GetManifestResourceStream(text))
+        using (var streamReader = new StreamReader(stream))
+        {
+            return streamReader.ReadToEnd();
+        }
     }
+
 }
