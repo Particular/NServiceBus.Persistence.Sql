5519168b01d4c3e9fddbed5a59700bf4ee26e5b1
Simon Cropp <simon.cropp@gmail.com>
2017-05-10 21:33:59 +1000

    add test holder

---
+++ src/AcceptanceTestsHolder/TestSuiteConstraints.cs
@@ -0,0 +1,15 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using AcceptanceTesting.Support;
+
+    public partial class TestSuiteConstraints
+    {
+        public bool SupportsDtc => true;
+        public bool SupportsCrossQueueTransactions => true;
+        public bool SupportsNativePubSub => false;
+        public bool SupportsNativeDeferral => false;
+        public bool SupportsOutbox => true;
+        public IConfigureEndpointTestExecution TransportConfiguration => null;
+        public IConfigureEndpointTestExecution PersistenceConfiguration => null;
+    }
+}
