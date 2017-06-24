c52213a7985e3932ead9318dd79ee3ebbd6225a8
Simon Cropp <simon.cropp@gmail.com>
2017-06-24 11:49:08 +1000

    core 6.3.4

---
+++ src/AcceptanceTestsHolder/App_Packages/NSB.AcceptanceTests.6.3.4/Sagas/When_saga_id_changed.cs
@@ -0,0 +1,73 @@
+﻿namespace NServiceBus.AcceptanceTests.Sagas
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Support;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_saga_id_changed : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_throw()
+        {
+            var exception = Assert.ThrowsAsync<MessageFailedException>(async () =>
+                await Scenario.Define<Context>()
+                    .WithEndpoint<Endpoint>(
+                        b => b.When(session => session.SendLocal(new StartSaga
+                        {
+                            DataId = Guid.NewGuid()
+                        })))
+                    .Done(c => c.FailedMessages.Any())
+                    .Run());
+
+            Assert.That(exception.ScenarioContext.FailedMessages, Has.Count.EqualTo(1));
+            Assert.That(((Context) exception.ScenarioContext).MessageId, Is.EqualTo(exception.FailedMessage.MessageId), "Message should be moved to errorqueue");
+            Assert.That(exception.FailedMessage.Exception.Message, Contains.Substring("A modification of IContainSagaData.Id has been detected. This property is for infrastructure purposes only and should not be modified. SagaType:"));
+        }
+
+        public class Context : ScenarioContext
+        {
+            public string MessageId { get; set; }
+        }
+
+        public class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class SagaIdChangedSaga : Saga<SagaIdChangedSaga.SagaIdChangedSagaData>,
+                IAmStartedByMessages<StartSaga>
+            {
+                public Context TestContext { get; set; }
+
+                public Task Handle(StartSaga message, IMessageHandlerContext context)
+                {
+                    Data.Id = Guid.NewGuid();
+                    TestContext.MessageId = context.MessageId;
+                    return Task.FromResult(0);
+                }
+
+                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaIdChangedSagaData> mapper)
+                {
+                    mapper.ConfigureMapping<StartSaga>(m => m.DataId).ToSaga(s => s.DataId);
+                }
+
+                public class SagaIdChangedSagaData : ContainSagaData
+                {
+                    public virtual Guid DataId { get; set; }
+                }
+            }
+        }
+
+        public class StartSaga : ICommand
+        {
+            public Guid DataId { get; set; }
+        }
+    }
+}
