1481f5126054320b87569211ebcfcdf3fa4a0f2f
Simon Cropp <simon.cropp@gmail.com>
2017-08-02 22:58:47 +1000

    add some tests to MySqlUnicode

---
+++ src/MySqlAcceptanceTests/TestPartials/When_headers_contain_special_characters.cs
@@ -0,0 +1,9 @@
+﻿namespace NServiceBus.AcceptanceTests.Outbox
+{
+    using NUnit.Framework;
+
+    [Category("MySqlUnicode")]
+    partial class When_headers_contain_special_characters
+    {
+    }
+}
---
+++ src/MySqlAcceptanceTests/TestPartials/When_using_special_characters_in_headers.cs
@@ -0,0 +1,9 @@
+﻿namespace NServiceBus.AcceptanceTests.DelayedDelivery
+{
+    using NUnit.Framework;
+
+    [Category("MySqlUnicode")]
+    partial class When_using_special_characters_in_headers
+    {
+    }
+}
