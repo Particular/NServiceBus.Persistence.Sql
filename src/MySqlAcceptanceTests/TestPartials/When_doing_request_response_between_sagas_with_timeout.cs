f70b9924adb8057612deef1fb1944dbb3459389b
Simon Cropp <simon.cropp@gmail.com>
2017-08-19 23:14:08 +1000

    redundant partials

--- src/MySqlAcceptanceTests/TestPartials/When_doing_request_response_between_sagas_with_timeout.cs
+++
@@ -1,9 +0,0 @@
-namespace NServiceBus.AcceptanceTests.Sagas
-{
-    using NUnit.Framework;
-
-    [Explicit]
-    public partial class When_doing_request_response_between_sagas_with_timeout
-    {
-    }
-}
