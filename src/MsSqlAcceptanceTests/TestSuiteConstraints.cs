3b234e1da4652759b982c1c1f6ec39236f62c7a8
Szymon Pobiega <szymon.pobiega@gmail.com>
2017-08-23 14:34:37 +0200

    Tests on core (#155)
    * Run tests on .net core
    
    * disabled core build for unit tests until approval tests have core edition
    
    * Updated to latest sql transport.
    
    * Fixed connection string.
    
    * Fixed inspections.
    
    * Fixed dtc tests.
    
    * Fixed last 2 tests.

--- src/MsSqlAcceptanceTests/TestSuiteConstraints.cs
+++ src/MsSqlAcceptanceTests/TestSuiteConstraints.cs
@@ -4,14 +4,18 @@
 
     public partial class TestSuiteConstraints
     {
+#if NET452
         public bool SupportsDtc => true;
+#else
+        public bool SupportsDtc => false;
+#endif
         public bool SupportsCrossQueueTransactions => true;
         public bool SupportsNativePubSub => false;
         public bool SupportsNativeDeferral => false;
         public bool SupportsOutbox => true;
         public IConfigureEndpointTestExecution CreateTransportConfiguration()
         {
-            return new ConfigureEndpointMsmqTransport();
+            return new ConfigureEndpointSqlServerTransport();
         }
 
         public IConfigureEndpointTestExecution CreatePersistenceConfiguration()
