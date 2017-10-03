230d1db53c9887123c00866ef51938e7b40a2917
Simon Cropp <simon.cropp@gmail.com>
2017-10-04 09:48:49 +1100

    make MySqlUnicode explicit in release

--- src/MySqlAcceptanceTests/TestPartials/When_using_special_characters_in_headers.cs
+++ src/MySqlAcceptanceTests/TestPartials/When_using_special_characters_in_headers.cs
@@ -1,8 +1,10 @@
 ﻿namespace NServiceBus.AcceptanceTests.DelayedDelivery
 {
+#if RELEASE
     using NUnit.Framework;
-
-    [Category("MySqlUnicode")]
+    // So this test does not run on CI as server install does not support unicode
+    [Explicit("MySqlUnicode")]
+#endif
     partial class When_using_special_characters_in_headers
     {
     }
