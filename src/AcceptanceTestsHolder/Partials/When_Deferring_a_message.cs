32b2fc7e972b6ef58226983452a2cd02944650cc
Simon Cropp <simon.cropp@gmail.com>
2017-10-04 11:04:00 +1100

    make partials easier to update

---
+++ src/AcceptanceTestsHolder/Partials/When_Deferring_a_message.cs
@@ -0,0 +1,9 @@
+﻿namespace NServiceBus.AcceptanceTests.DelayedDelivery
+{
+    using NUnit.Framework;
+
+    [Explicit("Flaky test that does not matter to sql")]
+    public partial class When_deferring_a_message
+    {
+    }
+}
