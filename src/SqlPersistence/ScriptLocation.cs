f400cd980b079c76db6d9fab3aaec6ef5f8a0085
Simon Cropp <simon.cropp@gmail.com>
2017-10-09 22:07:49 +1100

    missing save

--- src/SqlPersistence/ScriptLocation.cs
+++ src/SqlPersistence/ScriptLocation.cs
@@ -11,6 +11,17 @@ static class ScriptLocation
         return Path.Combine(currentDirectory, "NServiceBus.Persistence.Sql", dialect.Name);
     }
 
+    static string GetCurrentDirectory()
+    {
+        var entryAssembly = Assembly.GetEntryAssembly();
+        if (entryAssembly == null)
+        {
+            return AppDomain.CurrentDomain.BaseDirectory;
+        }
+        var codeBase = entryAssembly.CodeBase;
+        return Directory.GetParent(new Uri(codeBase).LocalPath).FullName;
+    }
+
     public static void ValidateScriptExists(string createScript)
     {
         if (!File.Exists(createScript))
