dddf13b829a4347a62539d902d23f9f48510af74
Simon Cropp <simon.cropp@gmail.com>
2017-10-11 15:08:54 +1100

    add PostgreSql to integration sample

---
+++ src/Integration/PostgreSqlSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Npgsql;
+using NpgsqlTypes;
+using NServiceBus;
+
+class Program
+{
+    static Task Main()
+    {
+        return EndpointStarter.Start("SqlPersistence.Sample.PostgreSql",
+            persistence =>
+            {
+                var sqlDialect = persistence.SqlDialect<SqlDialect.PostgreSql>();
+                sqlDialect.JsonBParameterModifier(parameter =>
+                {
+                    var npgsqlParameter = (NpgsqlParameter)parameter;
+                    npgsqlParameter.NpgsqlDbType = NpgsqlDbType.Jsonb;
+                });
+                persistence.TablePrefix("PostgreSql");
+                var connection = Environment.GetEnvironmentVariable("PostgreSqlConnectionString");
+                if (string.IsNullOrWhiteSpace(connection))
+                {
+                    throw new Exception("PostgreSqlConnectionString environment variable is empty");
+                }
+                persistence.ConnectionBuilder(() => new NpgsqlConnection(connection));
+            });
+    }
+}
