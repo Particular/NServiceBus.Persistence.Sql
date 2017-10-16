c0619c6559031f2c5018feaa0a3b7d9815a2ffaf
SzymonPobiega <szymon.pobiega@gmail.com>
2017-10-16 11:41:09 +0200

    Added code for enlisting into transport's transaction scope for PostgreSql

--- src/SqlPersistence/SynchronizedStorage/SqlDialect_PostgreSql.cs
+++ src/SqlPersistence/SynchronizedStorage/SqlDialect_PostgreSql.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Data.Common;
     using System.Threading.Tasks;
+    using System.Transactions;
     using Extensibility;
     using Persistence;
     using Transport;
@@ -11,12 +12,28 @@
     {
         public partial class PostgreSql
         {
-            static Task<CompletableSynchronizedStorageSession> result = Task.FromResult((CompletableSynchronizedStorageSession)null);
-
-            internal override Task<CompletableSynchronizedStorageSession> TryAdaptTransportConnection(TransportTransaction transportTransaction, ContextBag context, Func<DbConnection> connectionBuilder, Func<DbConnection, DbTransaction, bool, StorageSession> storageSessionFactory)
+            internal override async Task<CompletableSynchronizedStorageSession> TryAdaptTransportConnection(TransportTransaction transportTransaction, ContextBag context, Func<DbConnection> connectionBuilder, Func<DbConnection, DbTransaction, bool, StorageSession> storageSessionFactory)
             {
-                // PostgreSql does not support DTC so we should not enlist if transport has such a transaction.
-                return result;
+                // Transport supports DTC and uses TxScope owned by the transport
+                var scopeTx = Transaction.Current;
+                if (transportTransaction.TryGet(out Transaction transportTx) &&
+                    scopeTx != null &&
+                    transportTx != scopeTx)
+                {
+                    throw new Exception("A TransactionScope has been opened in the current context overriding the one created by the transport. "
+                                        + "This setup can result in inconsistent data because operations done via connections enlisted in the context scope won't be committed "
+                                        + "atomically with the receive transaction. To manually control the TransactionScope in the pipeline switch the transport transaction mode "
+                                        + $"to values lower than '{nameof(TransportTransactionMode.TransactionScope)}'.");
+                }
+                var ambientTransaction = transportTx ?? scopeTx;
+                if (ambientTransaction == null)
+                {
+                    //Other modes handled by creating a new session.
+                    return null;
+                }
+                var connection = await connectionBuilder.OpenConnection().ConfigureAwait(false);
+                connection.EnlistTransaction(ambientTransaction);
+                return storageSessionFactory(connection, null, true);
             }
         }
     }
