0f6a0c571392da1038ba599e2d1793e12fb8c71b
Simon Cropp <simon.cropp@gmail.com>
2017-10-24 11:02:45 +1100

    throw for JsonBParameterModifier missing

--- src/SqlPersistence/Config/SqlDialect_PostgreSql.cs
+++ src/SqlPersistence/Config/SqlDialect_PostgreSql.cs
@@ -25,8 +25,34 @@ namespace NServiceBus
 
             internal override void SetJsonParameterValue(DbParameter parameter, object value)
             {
+                ValidateJsonBModifier();
                 JsonBParameterModifier(parameter);
                 SetParameterValue(parameter, value);
+            }
+
+            void ValidateJsonBModifier()
+            {
+                if (JsonBParameterModifier != null)
+                {
+                    return;
+                }
+
+                var parameterProp = "NpgsqlParameter.NpgsqlDbType";
+                var jsonb = "NpgsqlDbType.Jsonb";
+                var parameterModifier = $"{nameof(SqlDialectSettings)}<{nameof(PostgreSql)}>.{nameof(SqlPersistenceConfig.JsonBParameterModifier)}()";
+                var error = $@"The {parameterModifier} method has not been set.
+Npgsql requires that parameters that passed JSONB explicitly have {parameterProp} set to {jsonb}.
+Npgsql does not infer this based on the column type.
+It is not possible for the Sql Persistence to control this setting while still avoiding a reference to Npgsql.
+As such it is necessary to explicitly set {parameterProp} to {jsonb} via a call to {parameterModifier}:
+
+var dialect = persistence.SqlDialect<SqlDialect.PostgreSql>();
+dialect.JsonBParameterModifier(parameter =>
+{{
+    var npgsqlParameter = (NpgsqlParameter)parameter;
+    npgsqlParameter.NpgsqlDbType = NpgsqlDbType.Jsonb;
+}});";
+                throw new Exception(error);
             }
 
             internal override void SetParameterValue(DbParameter parameter, object value)
@@ -44,10 +70,11 @@ namespace NServiceBus
 
             internal override void ValidateTablePrefix(string tablePrefix)
             {
-                if (tablePrefix.Length > 20)
+                if (tablePrefix.Length <= 20)
                 {
-                    throw new Exception($"Table prefix '{tablePrefix}' contains more than 20 characters, which is not supported by SQL persistence using PostgreSQL. Shorten the endpoint name or specify a custom tablePrefix using endpointConfiguration.{nameof(SqlPersistenceConfig.TablePrefix)}(tablePrefix).");
+                    return;
                 }
+                throw new Exception($"Table prefix '{tablePrefix}' contains more than 20 characters, which is not supported by SQL persistence using PostgreSQL. Shorten the endpoint name or specify a custom tablePrefix using endpointConfiguration.{nameof(SqlPersistenceConfig.TablePrefix)}(tablePrefix).");
             }
             internal string Schema { get; set; }
         }
