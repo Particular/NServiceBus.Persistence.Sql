f3afa78481cce1f40c6eb4d87ff00433bae8bc09
Simon Cropp <simon.cropp@gmail.com>
2017-10-31 14:01:05 +1100

    .

--- src/AcceptanceTestsHolder/App_Packages/NSB.AcceptanceTests/Sagas/When_replying_to_saga_event.cs
+++ src/AcceptanceTestsHolder/App_Packages/NSB.AcceptanceTests/Sagas/When_replying_to_saga_event.cs
@@ -6,6 +6,7 @@
     using EndpointTemplates;
     using Features;
     using NUnit.Framework;
+    using Persistence.Sql;
 
     public class When_replying_to_saga_event : NServiceBusAcceptanceTest
     {
@@ -70,9 +71,11 @@
                 });
             }
 
-            public class ReplyToPubMsgSaga : Saga<ReplyToPubMsgSaga.ReplyToPubMsgSagaData>, IAmStartedByMessages<StartSaga>, IHandleMessages<DidSomethingResponse>
+            public class ReplyToPubMsgSaga : SqlSaga<ReplyToPubMsgSaga.ReplyToPubMsgSagaData>, IAmStartedByMessages<StartSaga>, IHandleMessages<DidSomethingResponse>
             {
                 public Context Context { get; set; }
+
+                protected override string CorrelationPropertyName => nameof(ReplyToPubMsgSagaData.DataId);
 
                 public Task Handle(StartSaga message, IMessageHandlerContext context)
                 {
@@ -88,9 +91,9 @@
                     return Task.FromResult(0);
                 }
 
-                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<ReplyToPubMsgSagaData> mapper)
+                protected override void ConfigureMapping(IMessagePropertyMapper mapper)
                 {
-                    mapper.ConfigureMapping<StartSaga>(m => m.DataId).ToSaga(s => s.DataId);
+                    mapper.ConfigureMapping<StartSaga>(m => m.DataId);
                 }
 
                 public class ReplyToPubMsgSagaData : ContainSagaData
