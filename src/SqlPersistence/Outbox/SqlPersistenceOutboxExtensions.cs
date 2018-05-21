fe206df46538e6e642da27cfb3d3fd1c5b8cf9c1
Weronika Łabaj <weronika.labaj@particular.net>
2018-05-21 10:34:58 +0200

    Add settigs to: - disable the built-in cleanup process - specify cleanup task frequency interval - specify for how long deduplication data should be kept

---
+++ src/SqlPersistence/Outbox/SqlPersistenceOutboxExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace NServiceBus
+{
+    using Configuration.AdvancedExtensibility;
+    using System;
+
+    /// <summary>
+    /// Contains extensions methods which allow to configure SQL persistence specific outbox configuration
+    /// </summary>
+    public static class SqlPersistenceOutboxExtensions
+    {
+        /// <summary>
+        /// Sets the time to keep the deduplication data to the specified time span.
+        /// </summary>
+        /// <param name="configuration">The configuration being extended.</param>
+        /// <param name="timeToKeepDeduplicationData">The time to keep the deduplication data.
+        /// The cleanup process removes entries older than the specified time to keep deduplication data. The time span cannot be negative or zero.</param>
+        /// <returns>The configuration</returns>
+        public static void SetTimeToKeepDeduplicationData(this EndpointConfiguration configuration, TimeSpan timeToKeepDeduplicationData)
+        {
+            Guard.AgainstNull(nameof(configuration), configuration);
+            Guard.AgainstNegativeAndZero(nameof(timeToKeepDeduplicationData), timeToKeepDeduplicationData);
+
+            configuration.GetSettings().Set(SqlOutboxFeature.TimeToKeepDeduplicationData, timeToKeepDeduplicationData);
+        }
+
+        /// <summary>
+        /// Sets the frequency to run the deduplication data cleanup task.
+        /// </summary>
+        /// <param name="configuration">The configuration being extended.</param>
+        /// <param name="frequencyToRunDeduplicationDataCleanup">The frequency to run the deduplication data cleanup task. The time span cannot be negative or sero.</param>
+        /// <returns>The configuration</returns>
+        public static void SetFrequencyToRunDeduplicationDataCleanup(this EndpointConfiguration configuration, TimeSpan frequencyToRunDeduplicationDataCleanup)
+        {
+            Guard.AgainstNull(nameof(configuration), configuration);
+            Guard.AgainstNegativeAndZero(nameof(frequencyToRunDeduplicationDataCleanup), frequencyToRunDeduplicationDataCleanup);
+
+            configuration.GetSettings().Set(SqlOutboxFeature.FrequencyToRunDeduplicationDataCleanup, frequencyToRunDeduplicationDataCleanup);
+        }
+
+        /// <summary>
+        /// Disable the built-in outbox deduplication records cleanup.
+        /// </summary>
+        public static void DisableCleanup(this EndpointConfiguration configuration)
+        {
+            configuration.GetSettings().Set(SqlOutboxFeature.DisableCleanup, true);
+        }
+    }
+}
