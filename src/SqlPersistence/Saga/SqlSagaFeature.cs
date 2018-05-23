98ff576eb664ab28e5d447f75fbdb9d4a89130b1
Weronika Łabaj <weronika.labaj@gmail.com>
2018-05-23 13:16:01 +0200

    Add custom saga diagnostics info

--- src/SqlPersistence/Saga/SqlSagaFeature.cs
+++ src/SqlPersistence/Saga/SqlSagaFeature.cs
@@ -1,4 +1,7 @@
-﻿using NServiceBus.Features;
+﻿using System.Collections.Generic;
+using NServiceBus;
+using NServiceBus.Features;
+using NServiceBus.Persistence.Sql;
 
 class SqlSagaFeature : Feature
 {
@@ -9,5 +12,19 @@ class SqlSagaFeature : Feature
 
     protected override void Setup(FeatureConfigurationContext context)
     {
+        var settings = context.Settings;
+
+        var customJsonSettings = SagaSettings.GetJsonSerializerSettings(settings);
+        var versionSpecificJsonSettings = SagaSettings.GetVersionSettings(settings);
+        var customSagaWriter = SagaSettings.GetWriterCreator(settings);
+        var customSagaReader = SagaSettings.GetReaderCreator(settings);
+
+        settings.AddStartupDiagnosticsSection("NServiceBus.Persistence.Sql.Sagas", new Dictionary<string, object>
+        {
+            { nameof(customJsonSettings), customJsonSettings != null},
+            { nameof(versionSpecificJsonSettings), versionSpecificJsonSettings != null},
+            { nameof(customSagaWriter), customSagaWriter != null},
+            { nameof(customSagaReader), customSagaReader != null}
+        });
     }
 }
