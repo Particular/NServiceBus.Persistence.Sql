6bf91aa92fb278e5029a34fd8e9ca629f61c0a82
David Boike <david.boike@gmail.com>
2018-06-05 14:09:02 -0500

    Implementation of saga metadata reader for Core sagas

--- src/ScriptBuilder/Saga/SagaDefinitionReader.cs
+++ src/ScriptBuilder/Saga/SagaDefinitionReader.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
 using NServiceBus.Persistence.Sql;
 using NServiceBus.Persistence.Sql.ScriptBuilder;
 
@@ -7,7 +9,12 @@ static class SagaDefinitionReader
 {
     public static bool TryGetSagaDefinition(TypeDefinition type, out SagaDefinition definition)
     {
-        ValidateIsNotDirectSaga(type);
+        return TryGetSqlSagaDefinition(type, out definition)
+               || TryInferSagaDefinition(type, out definition);
+    }
+
+    static bool TryGetSqlSagaDefinition(TypeDefinition type, out SagaDefinition definition)
+    { 
         if (!IsSqlSaga(type))
         {
             definition = null;
@@ -17,7 +24,7 @@ static class SagaDefinitionReader
 
         var correlation = GetCorrelationPropertyName(type);
         var transitional = GetTransitionalCorrelationPropertyName(type);
-        var tableSuffix = GetTableSuffix(type);
+        var tableSuffix = GetSqlSagaTableSuffix(type);
 
         SagaDefinitionValidator.ValidateSagaDefinition(correlation, type.FullName, transitional, tableSuffix);
 
@@ -37,6 +44,192 @@ static class SagaDefinitionReader
         );
         return true;
     }
+
+    static bool TryInferSagaDefinition(TypeDefinition type, out SagaDefinition definition)
+    {
+        var baseType = type.BaseType as GenericInstanceType;
+        definition = null;
+
+        // Class must directly inherit from NServiceBus.Saga<T> so that no tricks can be pulled from an intermediate class
+        if (baseType == null || !baseType.FullName.StartsWith("NServiceBus.Saga") || baseType.GenericArguments.Count != 1)
+        {
+            return false;
+        }
+
+        CheckIsValidSaga(type);
+
+        string correlationId = null;
+        string transitionalId = null;
+        string tableSuffix = null;
+
+        var attribute = type.GetSingleAttribute("NServiceBus.Persistence.Sql.SqlSagaAttribute");
+        if (attribute != null)
+        {
+            var args = attribute.ConstructorArguments;
+            correlationId = (string)args[0].Value;
+            transitionalId = (string)args[1].Value;
+            tableSuffix = (string)args[2].Value;
+        }
+
+        var sagaDataType = GetSagaDataTypeFromSagaType(type);
+
+        if (correlationId == null)
+        {
+            correlationId = InferCorrelationId(type, sagaDataType);
+        }
+
+        SagaDefinitionValidator.ValidateSagaDefinition(correlationId, type.FullName, transitionalId, tableSuffix);
+
+        if (tableSuffix == null)
+        {
+            tableSuffix = type.Name;
+        }
+
+        definition = new SagaDefinition
+        (
+            correlationProperty: BuildConstraintProperty(sagaDataType, correlationId),
+            transitionalCorrelationProperty: BuildConstraintProperty(sagaDataType, transitionalId),
+            tableSuffix: tableSuffix,
+            name: type.FullName
+        );
+        return true;
+    }
+
+    static string InferCorrelationId(TypeDefinition type, TypeDefinition sagaDataType)
+    {
+        string correlationId = null;
+        var permissiveMode = true;
+
+        var sagaDataTypeName = sagaDataType.FullName;
+        var configureMethod = type.Methods.FirstOrDefault(m => m.Name == "ConfigureHowToFindSaga");
+        if (configureMethod == null)
+        {
+            throw new ErrorsException("Saga does not contain a ConfigureHowToFindSaga method.");
+        }
+
+        //For debugging
+        //var il = String.Join(Environment.NewLine, configureMethod.Body.Instructions.Select(i => i.ToString()).ToArray());
+
+        foreach (var instruction in configureMethod.Body.Instructions)
+        {
+            switch (instruction.OpCode.Code)
+            {
+                case Code.Call:
+                    var callMethod = instruction.Operand as MethodReference;
+                    if (callMethod == null)
+                    {
+                        throw new Exception("Can't determine method call type for MSIL instruction");
+                    }
+
+                    if (callMethod.DeclaringType.FullName == "System.Linq.Expressions.Expression")
+                    {
+                        if (validExpressionMethods.Contains(callMethod.Name))
+                        {
+                            continue;
+                        }
+                        if (permissiveMode && callMethod.Name == "Call")
+                        {
+                            continue;
+                        }
+                    }
+
+                    if (callMethod.DeclaringType.FullName == "System.Type" && callMethod.Name == "GetTypeFromHandle")
+                    {
+                        continue;
+                    }
+
+                    if (callMethod.DeclaringType.FullName == "System.Reflection.MethodBase" && callMethod.Name == "GetMethodFromHandle")
+                    {
+                        continue;
+                    }
+
+                    // Any other method call is not OK, bail out
+                    throw new ErrorsException("Unable to determine Saga correlation property because an unexpected method call was detected in the ConfigureHowToFindSaga method. (OpCode: call)");
+
+                case Code.Calli:
+                    // Don't know of any valid uses for this call type, bail out
+                    throw new ErrorsException("Unable to determine Saga correlation property because an unexpected method call was detected in the ConfigureHowToFindSaga method. (OpCode: calli)");
+
+                case Code.Callvirt:
+                    var virtMethod = instruction.Operand as MethodReference;
+                    if (virtMethod == null)
+                    {
+                        throw new Exception("Can't determine method call type for IL instruction");
+                    }
+
+                    // Call to mapper.ConfigureMapping<T>() is OK
+                    if (virtMethod.Name == "ConfigureMapping" && virtMethod.DeclaringType.FullName.StartsWith("NServiceBus.SagaPropertyMapper"))
+                    {
+                        // Disable Expression.Call to be very conservative in the ToSaga() section
+                        permissiveMode = false;
+                        continue;
+                    }
+
+                    // Call for .ToSaga() is OK
+                    if (virtMethod.Name == "ToSaga" && virtMethod.DeclaringType.FullName.StartsWith("NServiceBus.ToSagaExpression"))
+                    {
+                        // Re-enable expression calls now that ToSaga() section is complete
+                        permissiveMode = true;
+                        continue;
+                    }
+
+                    // Any other callvirt is not OK, bail out
+                    throw new ErrorsException("Unable to determine Saga correlation property because an unexpected method call was detected in the ConfigureHowToFindSaga method. (OpCode: callvirt)");
+
+                case Code.Ldtoken:
+                    var methodDefinition = instruction.Operand as MethodDefinition;
+
+                    // Some Ldtokens have operands of type TypeDefinition, for loading types
+                    if (methodDefinition == null)
+                    {
+                        continue;
+                    }
+
+                    // The method being loaded may be on wrong type, like getter for the message property
+                    if (methodDefinition.DeclaringType.FullName != sagaDataTypeName)
+                    {
+                        continue;
+                    }
+
+                    // If we're not getting a property, we're doing something unexpected, so bail out
+                    if (!methodDefinition.Name.StartsWith("get_"))
+                    {
+                        throw new ErrorsException("ToSaga() expression in Saga's ConfigureHowToFindSaga method should point to a saga data property.");
+                    }
+
+                    // methodDefinition.Name is the IL for the property, i.e. "get_Correlation"
+                    var instanceCorrelation = methodDefinition.Name.Substring(4);
+                    if (correlationId == null)
+                    {
+                        correlationId = instanceCorrelation;
+                    }
+                    else if (instanceCorrelation != correlationId)
+                    {
+                        throw new ErrorsException("Saga can only have one correlation property identified by .ToSaga() expressions. Fix mappings in ConfigureHowToFindSaga to map to a single correlation property or decorate the saga with [SqlSaga] attribute.");
+                    }
+                    break;
+
+                default:
+                    // Any branching logic is not OK
+                    if (instruction.OpCode.FlowControl == FlowControl.Branch)
+                    {
+                        throw new ErrorsException("Branching statements are not allowed in a ConfigureHowToFindSaga method.");
+                    }
+                    break;
+            }
+        }
+        return correlationId;
+    }
+
+    static readonly string[] validExpressionMethods = new[]
+    {
+        "Convert",
+        "Parameter",
+        "Property",
+        "Lambda",
+        "Add",
+        "Constant"
+    };
 
     static string GetCorrelationPropertyName(TypeDefinition type)
     {
@@ -67,7 +260,7 @@ When all messages are mapped using finders then use the following: protected ove
 For example: protected override string TransitionalCorrelationPropertyName => nameof(SagaData.TheProperty);");
     }
 
-    static string GetTableSuffix(TypeDefinition type)
+    static string GetSqlSagaTableSuffix(TypeDefinition type)
     {
         if (!type.TryGetProperty("TableSuffix", out var property))
         {
@@ -103,19 +296,6 @@ For example: protected override string TableSuffix => ""TheCustomTableSuffix"";"
         }
         return baseType.Scope.Name.StartsWith("NServiceBus.Persistence.Sql") &&
                baseType.FullName.StartsWith("NServiceBus.Persistence.Sql.SqlSaga");
-    }
-
-    static void ValidateIsNotDirectSaga(TypeDefinition type)
-    {
-        if (type.BaseType == null)
-        {
-            return;
-        }
-        var baseTypeFullName = type.BaseType.FullName;
-        if (baseTypeFullName.StartsWith("NServiceBus.Saga"))
-        {
-            throw new ErrorsException($"The type '{type.FullName}' inherits from NServiceBus.Saga which is not supported. Inherit from NServiceBus.Persistence.Sql.SqlSaga.");
-        }
     }
 
     static TypeDefinition GetSagaDataTypeFromSagaType(TypeDefinition sagaType)
---
+++ src/SqlPersistence/Saga/SqlSagaAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NServiceBus.Persistence.Sql
+{
+    /// <summary>
+    /// Exposes extra configuration options for storing <see cref="Saga{TSagaData}"/>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public sealed class SqlSagaAttribute : Attribute
+    {
+        /// <summary>
+        /// Identifies the Saga correlation property if it cannot be determined by inspecting the ConfigureHowToFindSaga method.
+        /// </summary>
+        public string CorrelationProperty { get; }
+
+        /// <summary>
+        /// Used to transition between different properties for saga correlation.
+        /// </summary>
+        public string TransitionalCorrelationProperty { get; }
+
+        /// <summary>
+        /// The name of the table to use when storing the current <see cref="Saga{TSagaData}"/>.
+        /// Will be appended to the value specified in <see cref="SqlPersistenceConfig.TablePrefix"/>.
+        /// </summary>
+        public string TableSuffix { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="correlationProperty">Identifies the Saga correlation property if it cannot be determined by inspecting the ConfigureHowToFindSaga method.</param>
+        /// <param name="transitionalCorrelationProperty">Used to transition between different properties for saga correlation.</param>
+        /// <param name="tableSuffix">
+        /// The name of the table to use when storing the current <see cref="Saga{TSagaData}"/>.
+        /// Will be appended to the value specified in <see cref="SqlPersistenceConfig.TablePrefix"/>.
+        /// </param>
+        public SqlSagaAttribute(
+            string correlationProperty = null,
+            string transitionalCorrelationProperty = null,
+            string tableSuffix = null)
+        {
+            CorrelationProperty = correlationProperty;
+            TransitionalCorrelationProperty = transitionalCorrelationProperty;
+            TableSuffix = tableSuffix;
+        }
+    }
+}
