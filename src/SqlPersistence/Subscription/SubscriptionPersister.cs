ec20be593e104e0ee1c52bf57eef78d9d672638f
David Boike <david.boike@gmail.com>
2019-02-14 14:50:15 -0600

    Unused ref

--- src/SqlPersistence/Subscription/SubscriptionPersister.cs
+++ src/SqlPersistence/Subscription/SubscriptionPersister.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Transactions;
