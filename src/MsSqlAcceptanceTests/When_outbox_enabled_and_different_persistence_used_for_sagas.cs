dc62564d0b4e183819cfa121a68e6017dcdbb785
Brandon Ording <bording@gmail.com>
2020-03-03 12:48:05 -0500

    Update test projects to NServiceBus 7.2.3 in release-4.6 (#429)
    * Update test projects to NServiceBus 7.2.3
    
    * Update SourcePackageVersion
    
    * Fix casing of field retrieved via reflection
    
    * adapts tests to different error message
    
    * adapts tests to different error message for Oracle
    
    * adapts tests to different error message for PostgreSql
    
    * adapts tests to different error message for MySql
    
    Co-authored-by: Mauro Servienti <mauro.servienti@gmail.com>

---
+++ src/MsSqlAcceptanceTests/When_outbox_enabled_and_different_persistence_used_for_sagas.cs
@@ -0,0 +1,69 @@
+﻿using NServiceBus;
+using NServiceBus.AcceptanceTesting;
+using NServiceBus.AcceptanceTests;
+using NServiceBus.AcceptanceTests.EndpointTemplates;
+using NServiceBus.Persistence.Sql;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+[TestFixture]
+public class When_outbox_enabled_and_different_persistence_used_for_sagas : NServiceBusAcceptanceTest
+{
+    [Test]
+    public void Should_throw_exception_at_startup()
+    {
+        var ex = Assert.ThrowsAsync<System.Exception>(async () =>
+        {
+            await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithDummySaga>()
+                .Done(c => c.Done)
+                .Run()
+                .ConfigureAwait(false);
+        });
+
+        StringAssert.StartsWith("Sagas and the Outbox need to use the same type of persistence. Saga persistence is configured to use InMemoryPersistence. Outbox persistence is configured to use SqlPersistence.", ex.Message);
+    }
+
+    public class Context : ScenarioContext
+    {
+        public bool SessionCreated { get; set; }
+        public bool Done { get; set; }
+        public string ExceptionMessage { get; set; }
+    }
+
+    public class EndpointWithDummySaga : EndpointConfigurationBuilder
+    {
+        public EndpointWithDummySaga()
+        {
+            EndpointSetup<DefaultServer>(c =>
+            {
+                c.EnableOutbox();
+                c.UsePersistence<InMemoryPersistence, StorageType.Sagas>();
+            });
+        }
+
+        //This saga is not used but required to activate the saga feature
+        public class DummySaga : SqlSaga<DummySagaData>, IAmStartedByMessages<DummyMessage>
+        {
+            protected override string CorrelationPropertyName => "Dummy";
+            protected override void ConfigureMapping(IMessagePropertyMapper mapper)
+            {
+                mapper.ConfigureMapping<DummyMessage>(m => null);
+            }
+
+            public Task Handle(DummyMessage message, IMessageHandlerContext context)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public class DummySagaData : ContainSagaData
+        {
+            public string Dummy { get; set; }
+        }
+    }
+
+    public class DummyMessage : IMessage
+    {
+    }
+}
