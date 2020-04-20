256ea0920292077a1547be795cfd1d5507064cb3
Andreas Öhlund <andreas.ohlund@particular.net>
2020-04-20 14:23:23 +0200

    Make sure to only invoke defaults once

--- src/SqlPersistence/SqlPersistence.cs
+++ src/SqlPersistence/SqlPersistence.cs
@@ -36,6 +36,13 @@
             });
             Defaults(s =>
             {
+                var defaultsAppliedSettingsKey = "NServiceBus.Persistence.Sql.DefaultApplied";
+
+                if (s.HasSetting(defaultsAppliedSettingsKey))
+                {
+                    return;
+                }
+
                 var dialect = s.GetSqlDialect();
                 var diagnostics = dialect.GetCustomDialectDiagnosticsInfo();
 
@@ -46,6 +53,8 @@
                 });
 
                 s.EnableFeatureByDefault<InstallerFeature>();
+
+                s.Set(defaultsAppliedSettingsKey, true);
             });
         }
 
