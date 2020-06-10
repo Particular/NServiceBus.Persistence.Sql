0d0928b61b46e63ee07e27386d947dcee58d5508
Brandon Ording <bording@gmail.com>
2020-06-10 12:49:10 -0400

    Use official package for ScriptDom

--- src/ScriptBuilder.Tests/SqlValidator.cs
+++ src/ScriptBuilder.Tests/SqlValidator.cs
@@ -1,28 +1,27 @@
-#if NETFRAMEWORK
-using Microsoft.SqlServer.TransactSql.ScriptDom;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-#endif
+using Microsoft.SqlServer.TransactSql.ScriptDom;
 
 public static class SqlValidator
 {
     public static void Validate(string sql)
     {
-#if NETFRAMEWORK
         var parser = new TSql140Parser(false);
         IList<ParseError> errors;
+
         using (var reader = new StringReader(sql))
         {
             parser.Parse(reader, out errors);
         }
+
         if (errors == null || errors.Count == 0)
         {
             return;
         }
+
         var message = $"Sql errors:{string.Join("\r\n", errors.Select(error => error.Message))}";
         throw new Exception(message);
-#endif
     }
 }
