2717d10f93c8afa56e4f1f320feee36d05579004
Szymon Pobiega <szymon.pobiega@gmail.com>
2020-07-28 07:30:16 +0200

    Remove copied SagaIdGenerator

--- src/SqlPersistence.PersistenceTests/PersistenceTestsConfiguration.cs
+++ src/SqlPersistence.PersistenceTests/PersistenceTestsConfiguration.cs
@@ -159,50 +159,5 @@
         {
             return Task.CompletedTask;
         }
-
-        class DefaultSagaIdGenerator : ISagaIdGenerator
-        {
-            public Guid Generate(SagaIdGeneratorContext context)
-            {
-                // intentionally ignore the property name and the value.
-                return CombGuid.Generate();
-            }
-        }
-
-        
-
-        static class CombGuid
-        {
-            /// <summary>
-            /// Generate a new <see cref="Guid" /> using the comb algorithm.
-            /// </summary>
-            public static Guid Generate()
-            {
-                var guidArray = Guid.NewGuid().ToByteArray();
-
-                var now = DateTime.UtcNow;
-
-                // Get the days and milliseconds which will be used to build the byte string
-                var days = new TimeSpan(now.Ticks - BaseDateTicks);
-                var timeOfDay = now.TimeOfDay;
-
-                // Convert to a byte array
-                // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333
-                var daysArray = BitConverter.GetBytes(days.Days);
-                var millisecondArray = BitConverter.GetBytes((long)(timeOfDay.TotalMilliseconds / 3.333333));
-
-                // Reverse the bytes to match SQL Servers ordering
-                Array.Reverse(daysArray);
-                Array.Reverse(millisecondArray);
-
-                // Copy the bytes into the guid
-                Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
-                Array.Copy(millisecondArray, millisecondArray.Length - 4, guidArray, guidArray.Length - 4, 4);
-
-                return new Guid(guidArray);
-            }
-
-            static readonly long BaseDateTicks = new DateTime(1900, 1, 1).Ticks;
-        }
     }
 }
