863fd5d501862e25462d54d2c3080fa6202a5af2
Daniel Marbach <daniel.marbach@particular.net>
2020-08-20 14:25:29 +0200

    Introduce proper disposal of OutboxTransaction in cause it faulted during begin (#506)
    * Introduce proper disposal of OutboxTransaction in cause it faulted during begin
    
    * refactoring `Begin` on `OutboxTransaction`
    
    Co-authored-by: Tomek Masternak <tomasz.masternak@particular.net>

--- src/SqlPersistence/Outbox/ISqlOutboxTransaction.cs
+++ src/SqlPersistence/Outbox/ISqlOutboxTransaction.cs
@@ -10,7 +10,7 @@ interface ISqlOutboxTransaction : OutboxTransaction
 
     // Prepare is deliberately kept sync to allow floating of TxScope where needed
     void Prepare(ContextBag context);
-    Task<OutboxTransaction> Begin(ContextBag context);
+    Task Begin(ContextBag context);
     Task Complete(OutboxMessage outboxMessage, ContextBag context);
     void BeginSynchronizedSession(ContextBag context);
 }
