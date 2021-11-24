346031ac82bdad9018ddc0d754987f37e2923742
Andreas Öhlund <andreas.ohlund@particular.net>
2021-11-24 15:52:20 +0100

    Moar

--- src/MsSqlSystemDataClientSqlTransportAcceptanceTests/ConfigureEndpointSqlServerTransport.cs
+++ src/MsSqlSystemDataClientSqlTransportAcceptanceTests/ConfigureEndpointSqlServerTransport.cs
@@ -179,7 +179,7 @@ public class ConfigureEndpointSqlServerTransport : IConfigureEndpointTestExecuti
         {
             var infra = await base.Initialize(hostSettings, receivers, sendingAddresses, cancellationToken);
 
-            //ReceiveAddresses = infra.Receivers.Select(r => r.Value.ReceiveAddress).ToArray();
+            ReceiveAddresses = infra.Receivers.Select(r => r.Value.ReceiveAddress).ToArray();
             return infra;
         }
     }
