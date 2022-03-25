9412b955f08db5d192283796a18cbfe73609a9da
Alexander Kucherov <50913857+alexander-kucherov@users.noreply.github.com>
2022-03-25 17:07:07 +0300

    add comment avout default TransactionOptions.Timeout
    Co-authored-by: Ramon Smits <ramon.smits@gmail.com>

--- src/SqlPersistence/Outbox/TransactionScopeSqlOutboxTransaction.cs
+++ src/SqlPersistence/Outbox/TransactionScopeSqlOutboxTransaction.cs
@@ -37,7 +37,7 @@ class TransactionScopeSqlOutboxTransaction : ISqlOutboxTransaction
         var options = new TransactionOptions
         {
             IsolationLevel = isolationLevel,
-            Timeout = transactionTimeout
+            Timeout = transactionTimeout; // TimeSpan.Zero is default of `TransactionOptions.Timeout`
         };
 
         transactionScope = new TransactionScope(TransactionScopeOption.RequiresNew, options, TransactionScopeAsyncFlowOption.Enabled);
