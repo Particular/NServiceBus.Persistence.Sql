ac3e021cf506467a4b22edf84132fa65821fee43
Daniel Marbach <daniel.marbach@openplace.net>
2022-04-29 09:38:31 +0200

    Move the connection factory to an extension method

--- src/SqlPersistence.PersistenceTests/SqlTestVariant.cs
+++ src/SqlPersistence.PersistenceTests/SqlTestVariant.cs
@@ -1,27 +1,20 @@
 ﻿namespace NServiceBus.PersistenceTesting
 {
-    using System;
-    using System.Data.Common;
     using Persistence.Sql.ScriptBuilder;
 
     class SqlTestVariant
     {
-        public SqlTestVariant(SqlDialect dialect, BuildSqlDialect buildDialect, Func<DbConnection> connectionFactory, bool usePessimisticMode)
+        public SqlTestVariant(SqlDialect dialect, BuildSqlDialect buildDialect, bool usePessimisticMode)
         {
             Dialect = dialect;
             BuildDialect = buildDialect;
-            ConnectionFactory = connectionFactory;
             UsePessimisticMode = usePessimisticMode;
         }
 
         public SqlDialect Dialect { get; }
         public BuildSqlDialect BuildDialect { get; }
-        public Func<DbConnection> ConnectionFactory { get; }
         public bool UsePessimisticMode { get; set; }
 
-        public override string ToString()
-        {
-            return $"{Dialect.GetType().Name}-pessimistic={UsePessimisticMode}";
-        }
+        public override string ToString() => $"{Dialect.GetType().Name}-pessimistic={UsePessimisticMode}";
     }
 }
