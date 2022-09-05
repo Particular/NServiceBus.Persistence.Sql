8fe1db45e097023cccb50a975b6400cab8e7bf84
lailabougria <laila.bougria@particular.net>
2022-09-05 13:25:48 +0200

    Add acceptance tests project for transactional session

---
+++ src/TransactionalSession.Sql.AcceptanceTests/ConfigureSqlPersistence.cs
@@ -0,0 +1,23 @@
+﻿namespace NServiceBus.AcceptanceTests;
+
+using System.Threading.Tasks;
+using AcceptanceTesting.Support;
+
+public class ConfigureSqlPersistence : IConfigureEndpointTestExecution
+{
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+    {
+        var persistence = configuration.UsePersistence<SqlPersistence>();
+        persistence.ConnectionBuilder(MsSqlMicrosoftDataClientConnectionBuilder.Build);
+        persistence.SqlDialect<SqlDialect.MsSqlServer>();
+        var subscriptions = persistence.SubscriptionSettings();
+        subscriptions.DisableCache();
+        persistence.DisableInstaller();
+
+        //persistence.EnableTransactionalSession();
+
+        return Task.CompletedTask;
+    }
+
+    Task IConfigureEndpointTestExecution.Cleanup() => Task.CompletedTask;
+}
