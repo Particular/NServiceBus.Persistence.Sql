ec8d1f85fd50d88be86eed3e48b184066418ee2f
danielmarbach <daniel.marbach@openplace.net>
2022-09-12 20:34:20 +0200

    PublicApiGenerator update for ScriptBuilder.Tests

--- src/ScriptBuilder.Tests/APIApprovals.cs
+++ src/ScriptBuilder.Tests/APIApprovals.cs
@@ -8,7 +8,10 @@ public class APIApprovals
     [Test]
     public void Approve()
     {
-        var publicApi = ApiGenerator.GeneratePublicApi(typeof(ScriptWriter).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" });
+        var publicApi = typeof(ScriptWriter).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+        });
         Approver.Verify(publicApi);
     }
 }
