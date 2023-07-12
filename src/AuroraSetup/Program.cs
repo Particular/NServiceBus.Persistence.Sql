ed3946880edd4ed5f197eb266dac843ed670d673
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-07-12 16:14:31 +0200

    formatting tweaks

--- src/AuroraSetup/Program.cs
+++ src/AuroraSetup/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Amazon.CDK;
+﻿using Amazon.CDK;
 using Amazon.CDK.AWS.EC2;
 using Amazon.CDK.AWS.RDS;
 using AuroraSetup;
@@ -10,25 +8,16 @@ var app = new App();
 
 var stack = new AuroraTestInfrastructure(app, "AuroraTestInfrastructure", new StackProps
 {
-    // If you don't specify 'env', this stack will be environment-agnostic.
-    // Account/Region-dependent features and context lookups will not work,
-    // but a single synthesized template can be deployed anywhere.
-
-    // Uncomment the next block to specialize this stack for the AWS Account
-    // and Region that are implied by the current CLI configuration.
-
+    // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     Env = new Amazon.CDK.Environment
     {
         Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
         Region = "us-east-2",
     }
-
-
-    // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 });
 
 var vpc = new Vpc(stack, "VPC",
-    new VpcProps()
+    new VpcProps
     {
         NatGateways = 0,
         SubnetConfiguration = new[]
@@ -46,28 +35,28 @@ var securityGroup = new SecurityGroup(stack, "SecurityGroup", new SecurityGroupP
 securityGroup.AddIngressRule(Peer.AnyIpv4(), Port.Tcp(5432), "allow CI for PostgreSQL connections");
 securityGroup.AddIngressRule(Peer.AnyIpv4(), Port.Tcp(3306), "allow CI for MySQL connections");
 
-var postgresCluster = new DatabaseCluster(stack, "PostgreSqlCluster", new DatabaseClusterProps()
+var postgresCluster = new DatabaseCluster(stack, "PostgreSqlCluster", new DatabaseClusterProps
 {
-    Engine = DatabaseClusterEngine.AuroraPostgres(new AuroraPostgresClusterEngineProps()
+    Engine = DatabaseClusterEngine.AuroraPostgres(new AuroraPostgresClusterEngineProps
     {
         Version = AuroraPostgresEngineVersion.VER_15_2
     }),
     Credentials = Credentials.FromGeneratedSecret("aurora_postgres", new CredentialsBaseOptions { SecretName = "aurora_postgres_secrets" }),
     Vpc = vpc,
     StorageType = DBClusterStorageType.AURORA_IOPT1, // is IO optimized better for tests?
-    Writer = ClusterInstance.Provisioned("Writer", new ProvisionedClusterInstanceProps()
+    Writer = ClusterInstance.Provisioned("Writer", new ProvisionedClusterInstanceProps
     {
         InstanceType = InstanceType.Of(InstanceClass.T3, InstanceSize.MEDIUM),
         PubliclyAccessible = true,
     }),
-    VpcSubnets = new SubnetSelection() { SubnetType = SubnetType.PUBLIC },
+    VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PUBLIC },
     SecurityGroups = new[]
     {
         securityGroup
     }
 });
 
-var mysqlCluster = new DatabaseCluster(stack, "MySqlCluster", new DatabaseClusterProps()
+var mysqlCluster = new DatabaseCluster(stack, "MySqlCluster", new DatabaseClusterProps
 {
     Engine = DatabaseClusterEngine.AuroraMysql(new AuroraMysqlClusterEngineProps
     {
@@ -81,14 +70,14 @@ var mysqlCluster = new DatabaseCluster(stack, "MySqlCluster", new DatabaseCluste
         InstanceType = InstanceType.Of(InstanceClass.T3, InstanceSize.MEDIUM),
         PubliclyAccessible = true
     }),
-    VpcSubnets = new SubnetSelection() { SubnetType = SubnetType.PUBLIC },
+    VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PUBLIC },
     SecurityGroups = new[]
     {
         securityGroup
     }
 });
 
-new CfnOutput(stack, "postgres_secrets", new CfnOutputProps() { Value = postgresCluster.Secret!.SecretName });
-new CfnOutput(stack, "mysql_secrets", new CfnOutputProps() { Value = mysqlCluster.Secret!.SecretName });
+_ = new CfnOutput(stack, "postgres_secrets", new CfnOutputProps { Value = postgresCluster.Secret!.SecretName });
+_ = new CfnOutput(stack, "mysql_secrets", new CfnOutputProps { Value = mysqlCluster.Secret!.SecretName });
 
 app.Synth();
