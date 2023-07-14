513b143ea5e8f7e05e18edca012a6e8e1ea1a573
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-07-14 13:35:05 +0200

    setup mysql before aurora

--- src/AuroraSetup/Program.cs
+++ src/AuroraSetup/Program.cs
@@ -40,27 +40,6 @@ var securityGroup = new SecurityGroup(stack, "SecurityGroup", new SecurityGroupP
 securityGroup.AddIngressRule(Peer.AnyIpv4(), Port.Tcp(5432), "allow CI for PostgreSQL connections");
 securityGroup.AddIngressRule(Peer.AnyIpv4(), Port.Tcp(3306), "allow CI for MySQL connections");
 
-var postgresCluster = new DatabaseCluster(stack, "PostgreSqlCluster", new DatabaseClusterProps
-{
-    Engine = DatabaseClusterEngine.AuroraPostgres(new AuroraPostgresClusterEngineProps
-    {
-        Version = AuroraPostgresEngineVersion.VER_15_2
-    }),
-    Credentials = Credentials.FromGeneratedSecret("aurora_postgres", new CredentialsBaseOptions { SecretName = "aurora_postgres_secrets" }),
-    Vpc = vpc,
-    StorageType = DBClusterStorageType.AURORA_IOPT1, // is IO optimized better for tests?
-    Writer = ClusterInstance.Provisioned("Writer", new ProvisionedClusterInstanceProps
-    {
-        InstanceType = InstanceType.Of(InstanceClass.T3, InstanceSize.MEDIUM),
-        PubliclyAccessible = true,
-    }),
-    VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PUBLIC },
-    SecurityGroups = new[]
-    {
-        securityGroup
-    }
-});
-
 var mysqlCluster = new DatabaseCluster(stack, "MySqlCluster", new DatabaseClusterProps
 {
     Engine = DatabaseClusterEngine.AuroraMysql(new AuroraMysqlClusterEngineProps
@@ -74,6 +53,27 @@ var mysqlCluster = new DatabaseCluster(stack, "MySqlCluster", new DatabaseCluste
     {
         InstanceType = InstanceType.Of(InstanceClass.T3, InstanceSize.MEDIUM),
         PubliclyAccessible = true
+    }),
+    VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PUBLIC },
+    SecurityGroups = new[]
+    {
+        securityGroup
+    }
+});
+
+var postgresCluster = new DatabaseCluster(stack, "PostgreSqlCluster", new DatabaseClusterProps
+{
+    Engine = DatabaseClusterEngine.AuroraPostgres(new AuroraPostgresClusterEngineProps
+    {
+        Version = AuroraPostgresEngineVersion.VER_15_2
+    }),
+    Credentials = Credentials.FromGeneratedSecret("aurora_postgres", new CredentialsBaseOptions { SecretName = "aurora_postgres_secrets" }),
+    Vpc = vpc,
+    StorageType = DBClusterStorageType.AURORA_IOPT1, // is IO optimized better for tests?
+    Writer = ClusterInstance.Provisioned("Writer", new ProvisionedClusterInstanceProps
+    {
+        InstanceType = InstanceType.Of(InstanceClass.T3, InstanceSize.MEDIUM),
+        PubliclyAccessible = true,
     }),
     VpcSubnets = new SubnetSelection { SubnetType = SubnetType.PUBLIC },
     SecurityGroups = new[]
