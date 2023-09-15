4e736b87b76116717182fe8c983bac02f9a1907b
Brandon Ording <bording@gmail.com>
2023-09-15 16:20:57 -0400

    Remove unused class

--- src/SqlPersistence.Tests/Requires.cs
+++
@@ -1,7 +0,0 @@
-class Requires
-{
-    public static void DtcSupport()
-    {
-        NUnit.Framework.Assert.Ignore("Ignoring this test because it requires DTC transaction support from the transport.");
-    }
-}
