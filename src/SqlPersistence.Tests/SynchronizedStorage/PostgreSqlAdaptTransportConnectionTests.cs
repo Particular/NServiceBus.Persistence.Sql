3c58b183908843ecbb5c685ac2be3f5d0a62cfbe
Tomek Masternak <tomasz.masternak@particular.net>
2024-05-27 12:17:05 +0200

    fixing postgres con/tx adaptation tests

--- src/SqlPersistence.Tests/SynchronizedStorage/PostgreSqlAdaptTransportConnectionTests.cs
+++ src/SqlPersistence.Tests/SynchronizedStorage/PostgreSqlAdaptTransportConnectionTests.cs
@@ -2,22 +2,72 @@ namespace PostgreSql
 {
     using System;
     using System.Data.Common;
+    using NServiceBus.Extensibility;
+    using System.Threading.Tasks;
+    using System.Transactions;
     using NServiceBus.Persistence.Sql.ScriptBuilder;
+    using NServiceBus.Transport;
+    using NUnit.Framework;
 
     [PostgreSqlTest]
-    class PostgreSqlAdaptTransportConnectionTests : AdaptTransportConnectionTests
+    class PostgreSqlAdaptTransportConnectionTests
     {
-        public PostgreSqlAdaptTransportConnectionTests() : base(BuildSqlDialect.PostgreSql)
+        readonly BuildSqlDialect sqlDialect = BuildSqlDialect.PostgreSql;
+        readonly IConnectionManager connectionManager = new ConnectionManager(() =>
         {
+            var connection = PostgreSqlConnectionBuilder.Build();
+            connection.Open();
+
+            return connection;
+        });
+
+        [Test]
+        public void It_throws_if_non_transport_transaction_scope_exists()
+        {
+            using (var scope = new TransactionScope(TransactionScopeOption.RequiresNew, TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var ex = Assert.ThrowsAsync<Exception>(async () =>
+                {
+                    var transportTransaction = new TransportTransaction();
+                    transportTransaction.Set(Transaction.Current);
+
+                    await sqlDialect.Convert().TryAdaptTransportConnection(transportTransaction, new ContextBag(),
+                        connectionManager);
+                });
+
+                StringAssert.StartsWith("PostgreSQL persistence should not be used with TransportTransactionMode equal to TransactionScope", ex.Message);
+            }
         }
 
-        protected override Func<string, DbConnection> GetConnection()
+        [Test]
+        public async Task It_returns_false_if_there_is_no_transaction_scope()
         {
-            return x =>
-            {
-                var connection = PostgreSqlConnectionBuilder.Build();
-                return connection;
-            };
+            var transportTransaction = new TransportTransaction();
+            (bool wasAdapted, DbConnection _, DbTransaction _, bool _) = await sqlDialect.Convert().TryAdaptTransportConnection(transportTransaction, new ContextBag(), connectionManager);
+
+            Assert.That(wasAdapted, Is.False);
+        }
+
+        [Test]
+        public async Task Adapts_transport_connection()
+        {
+            var transportTransaction = new TransportTransaction();
+
+            var transportConnection = connectionManager.BuildNonContextual();
+            var transaction = transportConnection.BeginTransaction();
+
+            transportTransaction.Set("System.Data.SqlClient.SqlConnection", transportConnection);
+            transportTransaction.Set("System.Data.SqlClient.SqlTransaction", transaction);
+
+            var altConnectionManager = new ConnectionManager(() => throw new Exception("Should not be called"));
+
+            (bool wasAdapted, DbConnection connection, DbTransaction tx, bool ownsTransaction) =
+                await sqlDialect.Convert().TryAdaptTransportConnection(transportTransaction, new ContextBag(), altConnectionManager);
+
+            Assert.That(wasAdapted, Is.True);
+            Assert.AreEqual(transportConnection, connection);
+            Assert.AreEqual(transaction, tx);
+            Assert.That(ownsTransaction, Is.False);
         }
     }
 }
