a05507a5ba4db5000f53ba5b0a80c08d993d1fff
Phil Bastian <phil.bastian@particular.net>
2025-09-25 09:53:46 +0800

    remove unnecessary using to make build happy

--- src/SqlPersistence/ManifestOutput.cs
+++ src/SqlPersistence/ManifestOutput.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json.Serialization;
 using NServiceBus;
 using NServiceBus.Features;
 using NServiceBus.Settings;
