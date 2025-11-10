a62c4e820d57cb754b8e88b6837ad6169dba2fc0
Andreas Öhlund <andreas.ohlund@particular.net>
2025-11-10 12:33:17 +0100

    Move diagnostics

--- src/SqlPersistence/SqlPersistence.cs
+++ src/SqlPersistence/SqlPersistence.cs
@@ -1,6 +1,5 @@
 ﻿namespace NServiceBus
 {
-    using Features;
     using Persistence;
 
     /// <summary>
@@ -11,20 +10,6 @@
         // constructor parameter is a temporary workaround until the public constructor is removed
         SqlPersistence(object _)
         {
-            Defaults(s =>
-            {
-                var dialect = s.GetSqlDialect();
-                var diagnostics = dialect.GetCustomDialectDiagnosticsInfo();
-
-                s.AddStartupDiagnosticsSection("NServiceBus.Persistence.Sql.SqlDialect", new
-                {
-                    dialect.Name,
-                    CustomDiagnostics = diagnostics
-                });
-
-                s.EnableFeature<InstallerFeature>();
-            });
-
             Supports<StorageType.Outbox, SqlOutboxFeature>();
             Supports<StorageType.Sagas, SqlSagaFeature>();
             Supports<StorageType.Subscriptions, SqlSubscriptionFeature>();
