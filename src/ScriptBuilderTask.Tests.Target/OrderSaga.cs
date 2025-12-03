9c75b990ce36713fb77ad41320b8200ddf13b63b
David Boike <david.boike@gmail.com>
2025-12-03 10:16:23 -0600

    Spacing fixes

--- src/ScriptBuilderTask.Tests.Target/OrderSaga.cs
+++ src/ScriptBuilderTask.Tests.Target/OrderSaga.cs
@@ -2,19 +2,15 @@
 using System.Threading.Tasks;
 using NServiceBus;
 
-public class OrderSaga :
-    Saga<OrderSaga.SagaData>,
+public class OrderSaga : Saga<OrderSaga.SagaData>,
     IAmStartedByMessages<StartOrder>
 {
     protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper) => mapper.MapSaga(s => s.OrderId).ToMessage<StartOrder>(m => m.OrderId);
 
     public Task Handle(StartOrder message, IMessageHandlerContext context) => Task.CompletedTask;
 
-    public class SagaData :
-        ContainSagaData
+    public class SagaData : ContainSagaData
     {
         public Guid OrderId { get; set; }
     }
-
-
 }
