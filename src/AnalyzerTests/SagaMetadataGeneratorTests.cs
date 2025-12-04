3c105417af9a67cf143a44fd03c2fe0762f36032
David Boike <david.boike@gmail.com>
2025-12-04 14:54:20 -0600

    Additional test for expression-bodied ConfigureHowToFindSaga method

--- src/AnalyzerTests/SagaMetadataGeneratorTests.cs
+++ src/AnalyzerTests/SagaMetadataGeneratorTests.cs
@@ -191,4 +191,33 @@ public class SagaMetadataGeneratorTests
             .Approve()
             .AssertRunsAreEqual();
     }
+
+    [Test]
+    public void ExpressionBodiedMethod()
+    {
+        var code = $$"""
+                     using System.Threading.Tasks;
+                     using NServiceBus;
+
+                     public class OrderSaga : Saga<OrderSagaData>, IAmStartedByMessages<StartOrder>
+                     {
+                         protected override void ConfigureHowToFindSaga(SagaPropertyMapper<OrderSagaData> mapper)
+                            => mapper.MapSaga(saga => saga.OrderId).ToMessage<StartOrder>(message => message.OrderId);
+
+                         public Task Handle(StartOrder message, IMessageHandlerContext context) => Task.CompletedTask;
+                     }
+                     public class OrderSagaData : ContainSagaData
+                     {
+                         public string OrderId { get; set; }
+                     }
+                     public record class StartOrder(string OrderId);
+                     """;
+
+        SourceGeneratorTest.ForIncrementalGenerator<SagaMetadataGenerator>()
+            .WithSource(code)
+            .AddReference(MetadataReference.CreateFromFile(typeof(SqlSagaAttribute).Assembly.Location))
+            .WithGeneratorStages("SagaDetails", "Collected")
+            .Approve()
+            .AssertRunsAreEqual();
+    }
 }
