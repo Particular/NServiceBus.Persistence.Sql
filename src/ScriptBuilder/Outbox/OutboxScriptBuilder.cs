54360c8e88baabde96f4c0cdf43e8dfac57676d2
Brandon Ording <bording@gmail.com>
2026-07-08 17:47:38 -0400

    SQL scripts do not have consistent line endings when created on non-Windows operating systems (#2038)
    * Ensure that scripts use the correct line endings
    
    * Add net472 shim

--- src/ScriptBuilder/Outbox/OutboxScriptBuilder.cs
+++ src/ScriptBuilder/Outbox/OutboxScriptBuilder.cs
@@ -7,31 +7,41 @@
     {
         public static void BuildCreateScript(TextWriter writer, BuildSqlDialect sqlDialect)
         {
-            writer.Write(ResourceReader.ReadResource(sqlDialect, "Outbox.Create"));
+            var resource = ResourceReader.ReadResource(sqlDialect, "Outbox.Create");
+            resource = resource.ReplaceLineEndings();
+
+            writer.Write(resource);
         }
 
         public static string BuildCreateScript(BuildSqlDialect sqlDialect)
         {
             var stringBuilder = new StringBuilder();
+
             using (var stringWriter = new StringWriter(stringBuilder))
             {
                 BuildCreateScript(stringWriter, sqlDialect);
             }
+
             return stringBuilder.ToString();
         }
 
         public static void BuildDropScript(TextWriter writer, BuildSqlDialect sqlDialect)
         {
-            writer.Write(ResourceReader.ReadResource(sqlDialect, "Outbox.Drop"));
+            var resource = ResourceReader.ReadResource(sqlDialect, "Outbox.Drop");
+            resource = resource.ReplaceLineEndings();
+
+            writer.Write(resource);
         }
 
         public static string BuildDropScript(BuildSqlDialect sqlDialect)
         {
             var stringBuilder = new StringBuilder();
+
             using (var stringWriter = new StringWriter(stringBuilder))
             {
                 BuildDropScript(stringWriter, sqlDialect);
             }
+
             return stringBuilder.ToString();
         }
     }
---
+++ src/ScriptBuilder/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿#if NETFRAMEWORK
+namespace NServiceBus.Persistence.Sql.ScriptBuilder;
+
+using System.Text.RegularExpressions;
+
+static class StringExtensions
+{
+    // Since NETFRAMEWORK is Windows-only (ignoring mono), we can safely assume we need LF -> CRLF
+    public static string ReplaceLineEndings(this string input) => Regex.Replace(input, "(?<!\r)\n", "\r\n");
+}
+#endif
